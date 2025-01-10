a398999ae2d93bc5463dea2992decbd84cb128d7
kevinthomas <kevvin.thomas@gmail.com>
2025-01-10 11:25:31 +0530

    fixes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,201 @@
-import React from 'react'
+"use client";
+import HeroTemplate from "@/components/HeroTemplate";
+import NavBar from "@/components/NavBar";
+import React, { useState } from "react";
+import img26 from "@/public/images/26.jpg";
+import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+import CustomFooter from "@/components/CustomFooter";
 
-const page = () => {
+const futuraMedium = localFont({
+  src: "../../public/fonts/futura/futura-medium.ttf",
+});
+const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600"] });
+
+const Page = () => {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+    request: "",
+    town: "",
+    country: "",
+    message: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle form submission logic (e.g., send data to an API)
+    console.log("Form submitted:", formData);
+    setSubmitted(true);
+    setFormData({
+      first_name: "",
+      last_name: "",
+      email: "",
+      phone: "",
+      request: "",
+      town: "",
+      country: "",
+      message: "",
+    });
+  };
+
   return (
     <div>
-        Page Under Construction.
+      <NavBar homePage={false} />
+      <div className="px-10">
+        <HeroTemplate
+          image={img26.src}
+          subtitle="GET IN TOUCH"
+          title="Contact Us"
+        />
+        <div className="flex items-center py-10 justify-evenly">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-[#0B1237] shadow-lg rounded-lg p-6 w-full max-w-lg"
+          >
+            <h2
+              className={`${futuraMedium.className} text-white text-2xl tracking-tighter`}
+            >
+              Get in Touch
+            </h2>
+            <p
+              className={`${inter.className} text-white text-sm tracking-tighter`}
+            >
+              You can reach out to us anytime.
+            </p>
+            {submitted && (
+              <p className="text-green-600 mb-4">
+                Thank you for your message! We will get back to you soon.
+              </p>
+            )}
+            <div className="mb-4 flex gap-4">
+              <input
+                type="text"
+                id="first_name"
+                name="first_name"
+                value={formData.first_name}
+                onChange={(e) => handleChange(e)}
+                required
+                className="mt-1 p-3 block w-full rounded-full border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="First Name"
+              />
+              <input
+                type="text"
+                id="last_name"
+                name="last_name"
+                value={formData.last_name}
+                onChange={(e) => handleChange(e)}
+                required
+                className="mt-1 block w-full rounded-full p-3 border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Last Name"
+              />
+            </div>
+            <div className="mb-4 flex gap-4">
+              <input
+                type="text"
+                id="town"
+                name="town"
+                value={formData.town}
+                onChange={(e) => handleChange(e)}
+                required
+                className="mt-1 p-3 block w-full rounded-full border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Town"
+              />
+              <input
+                type="text"
+                id="country"
+                name="country"
+                value={formData.country}
+                onChange={(e) => handleChange(e)}
+                required
+                className="mt-1 block w-full rounded-full p-3 border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Country"
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={(e) => handleChange(e)}
+                required
+                className="mt-1 block w-full rounded-full p-3 border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Your email"
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="phone"
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={(e) => handleChange(e)}
+                required
+                className="mt-1 block w-full rounded-full p-3 border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Phone Number"
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                id="request"
+                name="request"
+                value={formData.request}
+                onChange={(e) => handleChange(e)}
+                required
+                className="mt-1 block w-full rounded-full p-3 border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Request"
+              />
+            </div>
+            <div className="mb-4">
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={(e) => handleChange(e)}
+                required
+                rows={4}
+                className="mt-1 block w-full rounded-xl p-3 border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Your message"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-[#2341FF] text-white font-medium py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Submit
+            </button>
+          </form>
+          <div className="flex flex-col items-center text-center gap-10">
+            <div className="flex flex-col gap-5">
+            <h2 className={`${futuraMedium.className} text-3xl tracking-tighter`}>Contact Information</h2>
+            <p className={`${inter.className} flex flex-col tracking-tighter`}>
+                <a href="mailto:stage@agsracing.com">stage@agsracing.com</a>
+                <a href="tel:+33(0)648686912">+33 (0) 6 48 68 69 12</a>
+            </p>
+            </div>
+            <div className="flex flex-col gap-5">
+            <h2 className={`${futuraMedium.className} text-3xl tracking-tighter`}>How to Find Us</h2>
+            <div><iframe width="100%" height="300" scrolling="no" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=AGS%20Formule%201+(AGS%20Formule%201)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps tracker sport</a></iframe></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <CustomFooter/>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
